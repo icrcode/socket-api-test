1d6ec898e608deaef60d5b7242b2ea9441f8b359
Ícaro <icarobotelhosocial@gmail.com>
2023-06-06 21:37:32 -0300

    -Volume 2

---
+++ src/br/org/catolicasc/client/GreetClient.java
@@ -0,0 +1,20 @@
+package br.org.catolicasc.client;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class GreetClient {
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    public void start(String ip, int port){
+
+    }
+    public void stop(){
+
+    }
+    public String sedMessage(String msg){
+        return null;
+    }
+}
