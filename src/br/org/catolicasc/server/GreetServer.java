4bc8b2231f7ac2ec4089a5b5c70aedbb6aa468b9
Ícaro <icarobotelhosocial@gmail.com>
2023-06-06 21:30:04 -0300

    -Volume 1

--- src/br/org/catolicasc/server/GreetServer.java
+++ src/br/org/catolicasc/server/GreetServer.java
@@ -1,2 +1,54 @@
-package br.org.catolicasc.server;public class GreetServer {
+package br.org.catolicasc.server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class GreetServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+    public void start(int port) throws IOException {
+        // inicializar atributos
+        serverSocket =new ServerSocket(port); //escuta na porta port
+        clientSocket = serverSocket.accept(); //espera conexao
+        //handler para escrita de dados
+        out = new PrintWriter(clientSocket.getOutputStream());
+        //handler para leitura de dados
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        clienthandler();
+    }
+    private void clienthandler() throws IOException {
+        String greeting = in.readLine();
+        if ("hello server".equals(greeting)){
+            out.println("hello client");
+        }
+    }
+    public void stop(){
+        try {
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        }catch (IOException ex){
+            System.out.println("Erro ao fechar a conexão.");
+        }
+
+    }
+    public static void main(String[] args){
+        GreetServer server = new GreetServer();
+        try {
+            server.start(12345);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }finally {
+            server.stop();
+        }
+    }
+
 }
